ece3b39c8006645ea614da3e6666abbb25bafdbc
John Pavek <JohnQPavek@gmail.com>
2017-09-18 08:08:00 -0500

    Cleaning file structure

--- ViewModels/BaseViewModel.js
+++ ViewModels/BaseViewModel.js
@@ -1,11 +1,11 @@
-function BaseViewModel () {
+function BaseViewModel() {
     var self = this;
 
-    self.Content = ko.observable({name: "Blog"});
+    self.Content = ko.observable({ name: "BlogTemplate" });
 
-    self.ChangeContent = function(content) {
-        return function() {
-            self.Content({name: content});
+    self.ChangeContent = function (content) {
+        return function () {
+            self.Content({ name: content + "Template", data: content + "ViewModel" });
         }
     }
 
@@ -14,7 +14,15 @@ function BaseViewModel () {
     self.ShowAdmin = self.ChangeContent("Admin");
     self.ShowBreakout = self.ChangeContent("Breakout");
     self.ShowNetherCalc = self.ChangeContent("NetherCalc");
-    self.ShowRecipeTool=self.ChangeContent("RecipeTool")
-}	
+    self.ShowRecipeTool = self.ChangeContent("RecipeTool");
+
+    self.Load = function(){
+        self.ChangeContent("Blog");
+
+    }
+
+    self.Load();
+
+}
 
 
--- ViewModels/RecipeToolViewModel.js
+++ ViewModels/RecipeToolViewModel.js
@@ -1,27 +1,27 @@
-function IngredientModel (data) {
-  var self = this;
-  
-  if (data == null) data = {};
+function IngredientModel(data) {
+    var self = this;
 
-  self.Id = ko.observable(data.Id);
-  self.Name = ko.observable(data.Name);
-  self.Quantity = ko.observable(data.Quantity);
-  
+    if (data == null) data = {};
+
+    self.Id = ko.observable(data.Id);
+    self.Name = ko.observable(data.Name);
+    self.Quantity = ko.observable(data.Quantity);
+
 }
 
-funtion MenuItemModel(data) {
-  var self = this;
+function MenuItemModel(data) {
+    var self = this;
 }
 
 function RecipeToolViewModel() {
-  var self = this;
+    var self = this;
 
 
-  self.Load = function() {
-    
+    self.Load = function () {
 
-  }
 
-  self.Load();
+    }
+
+    self.Load();
 
 }
